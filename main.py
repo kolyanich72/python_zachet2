b7106e2bbc606d6dd44bfbc5747cce252f969810
kolyanich72 <kolyanich72@gmail.com>
2023-05-18 20:42:58 +0300

    p2

--- main.py
+++ main.py
@@ -1,5 +1,64 @@
+from faker import Faker
+import json
+from conf import MODEL as M
+from random import randint as rndm_n, uniform as rndm_flt
+
+fk = Faker('ru_RU')
+#fk = Faker('en_US')
+
+
+def pk_(counter_pk=1):
+    """
+    устанавливается начальное значение через аргумент функции-генератора на момент инициализации.
+    :param counter_pk:
+    :return:
+    """
+    while True:
+        yield counter_pk
+        counter_pk += 1
+
+
+def gen_title():
+    """генерация названия книги
+    :return:str
+    """
+    return fk.text(max_nb_chars=20)
+
+
+def gen_isbn():
+    """генерация м/н номера isbn13 книги
+    :return: int
+    """
+    return fk.isbn13()
+
+
+def gen_author(x: int):
+    """генерация списка авторов книги
+    :param количество авторов = int
+    :return: список авторов =list
+    """
+    aut_list = [fk.name() for i in range(x)]
+    return aut_list
+
+
+def f_book_gen():
+    title = gen_title()
+    author = gen_author(rndm_n(1, 3))
+    bk = {'title': title, 'year': rndm_n(1800, 2023), 'pages':  rndm_n(1, 500),
+          'isbn13':  gen_isbn(), 'rating': rndm_n(0, 5),
+          'price': round(rndm_flt(1, 10000), 2), 'author': author
+          }
+    return bk
+
+
+def initial():
+    with open('json_book_file', 'w', encoding='utf-8') as f1:
+        for _ in range(1, 101):
+            pk = next(pk_(_))
+            data = {'model': M, 'pk': pk, 'fields': f_book_gen()}
+            f1.write(json.dumps(data, indent=4, ensure_ascii=False))
+            #json.dump(data, f1,  indent=4, ensure_ascii=False)
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
+    initial()
